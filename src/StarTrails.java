1ff10538a40b1e24648605f72ea06dd8a5df6e83
Ryan Zhu <36178918+alfphaderp@users.noreply.github.com>
2020-03-18 21:42:30 -0700

    Add comments detailing star trail options

--- src/StarTrails.java
+++ src/StarTrails.java
@@ -12,11 +12,15 @@ public class StarTrails extends PApplet {
 	}
 	
 	public void setup() {
+		// frame rate cap
 		frameRate(30);
 		
 		new StarTrailGenerator(this)
+			// number of stars to create
 			.setQuantity(500)
 			.setKinematicsSupplier(() -> {
+				// generate stars randomly in a square with the width of the screen, centered at the origin,
+				// then translate cartesian into polar
 				float halfWidth = width / 2;
 				
 				float x = random(-halfWidth, halfWidth);
@@ -25,21 +29,30 @@ public class StarTrails extends PApplet {
 				float r = sqrt(x * x + y * y);
 				float t = atan2(y, x);
 				
+				// initial polar position vector
 				PVector pos = new PVector(r, t);
+				// initial polar velocity vector
 				PVector vel = new PVector(0, 0.005f);
+				// initial polar acceleration vector
 				PVector acc = new PVector(0, 0);
 				
 				return new Kinematics(pos, vel, acc);
 			})
 			.setStarOptionsSupplier(() -> {
+				// number of past positions to remember
 				int historySize = 80;
+				// number of lines to draw for each star; higher = better quality but lower performance
 				int linesPerStar = 10;
+				// color of the star
 				int color = color(
 					random(180, 255),
 					random(150, 255),
 					255
 				);
+				// initial stroke width of the first line
 				float initStrokeWidth = 7.5f;
+				// stroke width decays geometrically per frame, this variable is the multiplier by which it decays
+				// lower = faster decay, higher = slower decay, 1 = no decay, >1 = geometric growth instead of decay
 				float strokeWidthDecay = 0.975f;
 				
 				return new StarTrailOptions(historySize, linesPerStar, color, initStrokeWidth, strokeWidthDecay);
